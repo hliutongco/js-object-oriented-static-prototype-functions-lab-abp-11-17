7adac81e9ce08d7d685db08dfa61505a5234e19a
Helen Liutongco <bellpickle@gmail.com>
2017-11-13 02:12:28 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,7 +2,7 @@ function IcebreakerResponse(email){
   this.email = email;
 }
 
-IcebreakerResponse.BatchCreate(emails){
+IcebreakerResponse.BatchCreate{
   return emails.map(function (email) {
     return new IcebreakerResponse(email[0]);
   })
